3e97998d5f27ac3a4738d0f92d70ce4a3b354caa
Katrin <katrinivanovaa@yahoo.com>
2025-12-29 22:18:55 +0000

    project complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("hello");
+showDate = (event) => {
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
+
+    let cityName = event.target.options[event.target.selectedIndex].text;
+
+    alert(`It is ${currentTime} in ${cityName}`);
+  }
+};
+
+let currentTimeElement = document.querySelector("#countries-dropdown-menu");
+currentTimeElement.addEventListener("change", showDate);
